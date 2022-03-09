eadec64287cc9bd048e506ef37ca7d5126a680c0
DanielStacey21 <100240561+DanielStacey21@users.noreply.github.com>
2022-03-09 13:54:18 -0600

    Add files via upload

---
+++ Module_1/Module_1/Module_1.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Module_1
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("Press Enter Key to Exit.")
+        Console.ReadLine()
+    End Sub
+End Module
